e5ec825f63d31d18fb0cb57af0bed0c4a20737db
Y.M. Cheung <y.m.cheung@YMs-MacBook-Pro.local>
2022-05-01 14:56:20 +0200

    add connect wallet

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,86 @@
 <template>
-  <router-view />
+  <div v-if="!isWalletConnect" class="q-ma-md">
+    <q-btn
+      class="full-width"
+      flat
+      color="black"
+      label="Connect your metamask wallet"
+      @click="connectWallet"
+    />
+  </div>
+  <div v-else>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      isWalletConnect: false,
+    };
+  },
+  created() {
+    this.connectWallet();
+  },
+  methods: {
+    async connectWallet() {
+      try {
+        const { ethereum } = window;
+        if (!ethereum) {
+          alert("Must connect to MetaMask!");
+          return;
+        }
+        const myAccounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        if (myAccounts.length) {
+          this.isWalletConnect = true;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<!-- <script>
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "App",
+  setup() {
+    let isWalletConnect = ref(false);
+
+    async function connectWallet() {
+      try {
+        const { ethereum } = window;
+        if (!ethereum) {
+          alert("Must connect to MetaMask!");
+          return;
+        }
+        const myAccounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        if (myAccounts.length) {
+          isWalletConnect.value = true;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    // created lifecycle
+    connectWallet();
+
+    return {
+      isWalletConnect,
+      connectWallet,
+    };
+  },
 });
-</script>
+</script> -->
