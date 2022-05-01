c8545e817cb3988d54fd386249e3cd746ffa273a
Y.M. Cheung <y.m.cheung@YMs-MacBook-Pro.local>
2022-05-01 15:35:35 +0200

    update smart contract

--- contracts/PokemonCard.sol
+++ contracts/PokemonCard.sol
@@ -5,17 +5,19 @@ pragma solidity >=0.7.0 <0.9.0;
 import "hardhat/console.sol";
 
 contract PokemonCard {
+    event Transfer(string id, string name, string series, string img, uint amount, address buyer, uint256 timestamp);
+
     struct Card {
-        uint256 id; // the card id of the pokemon card
+        string id; // the card id of the pokemon card
         string name; // title of pokemoncard
-        string series; // // from which series
-        uint256 amount; // the price to pay for the card
+        string series; // from which series
+        string img; // img url
+        uint amount; // the price to pay for the card
         address buyer; // msg.sender of the buyer of the card 
         uint256 timestamp; // The timestamp when the user waved.
     }
 
     Card[] public cards;
-
     address payable public owner;
 
     constructor() payable {
@@ -23,25 +25,18 @@ contract PokemonCard {
         console.log("Yo yo, I am a contract am I am smart");
     }
 
-     modifier onlyOwner () {
-       require(msg.sender == owner, "This can only be called by the contract owner!");
-       _;
-     }
 
-
-    function buyCard(uint256 _id, string calldata _name, string calldata _series, uint256 _amount) payable public {
-        Card memory card = Card(_id, _name, _series, _amount, msg.sender, block.timestamp);
+    function buyCard(string calldata _id, string calldata _name, string calldata _series, string calldata _img, uint _amount) payable public {
+        Card memory card = Card(_id, _name, _series, _img, _amount, msg.sender, block.timestamp);
         cards.push(card);
-
-        // 1, "charizard", "test series", 1
-        uint256 amount = _amount * (10 ** 18); // maybe not needed anymore after this is implemented into the contract, problems with the .5
-        // maybe the solution: https://ethereum.stackexchange.com/questions/119727/ethers-js-error-invalid-bignumber-string
-        require (msg.value == amount, "Amount is not the same");
+        
+        uint amount = _amount;
+       
         require(amount <= address(this).balance, "Trying to withdraw more money than they contract has.");
         (bool success, ) = (msg.sender).call{value: amount}("");
         require(success, "Failed to withdraw money from contract.");
+        emit Transfer(_id, _name, _series, _img, _amount, msg.sender, block.timestamp);
     }
-
 
     function getAllBoughtCards() public view returns (Card[] memory) {
         return cards;
